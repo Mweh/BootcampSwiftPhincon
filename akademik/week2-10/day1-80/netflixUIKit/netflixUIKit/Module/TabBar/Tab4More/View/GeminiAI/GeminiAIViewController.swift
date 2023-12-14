5bf9cc560ebd76b6da79fdcef4f776cf80b218bb
Mweh <fahmianton@gmail.com>
2023-12-14 08:07:50 +0700

    Added AIGemini for future plan

---
+++ akademik/week2-10/day1-80/netflixUIKit/netflixUIKit/Module/TabBar/Tab4More/View/GeminiAI/GeminiAIViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GeminiAIViewController.swift
+//  netflixUIKit
+//
+//  Created by Muhammad Fahmi on 14/12/23.
+//
+
+import GoogleGenerativeAI
+import UIKit
+
+class GeminiAIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        setupGeminiAI()
+    }
+
+//    func setupGeminiAI(){
+////        let palmClient = GenerativeLanguage(apiKey: "AIzaSyC8YxXTa8Gwfxkm2UdBt8HHNzJvKOHbQIQ")
+//////        let model = GenerativeModel(name: "MODEL_NAME", apiKey: APIKey.default)
+//////        let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+////
+////        let prompt = "Write a story about a magic backpack."
+////        let response = try await palmClient(prompt)
+////        if let text = response.text {
+////          print(text)
+////        }
+//        let model = GenerativeModel(name: "gemini-pro-vision", apiKey: "API_KEY")
+//        let cookieImage = UIImage(...)
+//        let prompt = "Do these look store-bought or homemade?"
+//
+//        let response = try await model.generateContent(prompt, cookieImage)
+//    }
+}
