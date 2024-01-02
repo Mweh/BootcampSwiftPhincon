142ae7df58db1e7e72e28df120ad6e98d686427b
Mweh <fahmianton@gmail.com>
2024-01-02 16:39:52 +0700

    upload techStack.png

---
+++ akademik/week2-10/day1-80/netflixUIKit/netflixUIKit/Module/Additional/MsFlixyAssistance/DateUIHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  DateUIHeaderView.swift
+//  netflixUIKit
+//
+//  Created by Muhammad Fahmi on 02/01/24.
+//
+
+import Foundation
---
+++ akademik/week2-10/day1-80/netflixUIKit/netflixUIKit/Module/Additional/MsFlixyAssistance/Model/MsFlixyModel.swift
@@ -0,0 +1,8 @@
+//
+//  MsFlixyModel.swift
+//  netflixUIKit
+//
+//  Created by Muhammad Fahmi on 02/01/24.
+//
+
+import Foundation
